a2325b249ddded1e98ced9c712e6237d41db8ecf
wingz <wingz@LAPTOP-JQT0RDEJ>
2022-01-20 13:55:53 +0800

    test commit

---
+++ TestConnectGit/src/testgitpackage/testConnectGit.java
@@ -0,0 +1,10 @@
+package testgitpackage;
+
+public class testConnectGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("connecting to Github");
+	}
+
+}
